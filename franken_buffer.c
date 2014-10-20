548b86238e4c41a956fa7da6b818c4ee1eff9031
Colin Waddell <mrcolin@gmail.com>
2014-10-20 18:02:51 +0100

    Update franken_buffer.c

--- franken_buffer.c
+++ franken_buffer.c
@@ -1,10 +1,10 @@
 struct FKNBUFFER *fkn_buffer_setup(struct FKNBUFFER *storage){
   int i;
 
-  for (i=0; i<OUTTER_BUFF_SIZE; i++)
+  for (i=0; i<FKN_OUTTER_BUFF_SIZE; i++)
   {
       // Link next item, or wrap-round to start if at the end
-      storage[i].next = (i==OUTTER_BUFF_SIZE-1) ? storage : &(storage[i+1]);
+      storage[i].next = (i==FKN_OUTTER_BUFF_SIZE-1) ? storage : &(storage[i+1]);
       storage[i].head =  storage;
       storage[i].buffer = malloc(sizeof(struct FKN_RINGBUFFER));
       storage[i].buffer->index = 0;
@@ -15,15 +15,15 @@ struct FKNBUFFER *fkn_buffer_setup(struct FKNBUFFER *storage){
 
 // LOOK - No ifs or buts!
 void fkn_buffer_add(struct FKNBUFFER **buffer, unsigned char value){
-  (*buffer)->buffer->data[((*buffer)->buffer->index++)&INNER_BUFF_SIZE_MASK] = value;
+  (*buffer)->buffer->data[((*buffer)->buffer->index++)&FKN_INNER_BUFF_SIZE_MASK] = value;
   (*buffer) = (*buffer)->next;
 }
 
 // Very unoptimised.
 char fkn_buffer_get(struct FKNBUFFER *buffer, unsigned int index){
-  struct CSBUFFER *buf = &(buffer->head[index % OUTTER_BUFF_SIZE]);
+  struct CSBUFFER *buf = &(buffer->head[index % FKN_OUTTER_BUFF_SIZE]);
   struct RINGBUFFER *rb = buf->buffer;
-  return rb->data[index / OUTTER_BUFF_SIZE];
+  return rb->data[index / FKN_OUTTER_BUFF_SIZE];
 }
 
 void fkn_buffer_clear(struct FKNBUFFER *buffer){
