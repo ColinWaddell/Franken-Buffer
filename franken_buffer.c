a1ffe4f2a3307a52c747b9aaac1cfbb1b279b593
Colin Waddell <mrcolin@gmail.com>
2014-10-20 17:55:13 +0100

    Update franken_buffer.c

--- franken_buffer.c
+++ franken_buffer.c
@@ -1,29 +1,29 @@
-struct CSBUFFER *buffer_setup(struct CSBUFFER *storage){
+struct FKNBUFFER *buffer_setup(struct FKNBUFFER *storage){
   int i;
 
   for (i=0; i<OUTTER_BUFF_SIZE; i++)
   {
       storage[i].next = (i==OUTTER_BUFF_SIZE-1) ? storage : &(storage[i+1]);
       storage[i].head =  storage;
-      storage[i].buffer = malloc(sizeof(struct RINGBUFFER));
+      storage[i].buffer = malloc(sizeof(struct FKN_RINGBUFFER));
       storage[i].buffer->index = 0;
     }
 
   return storage;
 }
 
-void buffer_add(struct CSBUFFER **buffer, unsigned char value){
+void buffer_add(struct FKNBUFFER **buffer, unsigned char value){
   (*buffer)->buffer->data[((*buffer)->buffer->index++)&INNER_BUFF_SIZE_MASK] = value;
   (*buffer) = (*buffer)->next;
 }
 
-char buffer_get(struct CSBUFFER *buffer, unsigned int index){
+char buffer_get(struct FKNBUFFER *buffer, unsigned int index){
   struct CSBUFFER *buf = &(buffer->head[index % OUTTER_BUFF_SIZE]);
   struct RINGBUFFER *rb = buf->buffer;
   return rb->data[index / OUTTER_BUFF_SIZE];
 }
 
-void buffer_clear(struct CSBUFFER *buffer){
+void buffer_clear(struct FKNBUFFER *buffer){
   unsigned int i, j;
 
   for (i=0; i<OUTTER_BUFF_SIZE; i++)
