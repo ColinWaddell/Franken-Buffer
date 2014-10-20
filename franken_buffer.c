f5d3fcfba02e7b65cfc835bf996bb31a499f99aa
Colin Waddell <mrcolin@gmail.com>
2014-10-20 17:57:54 +0100

    Update franken_buffer.c

--- franken_buffer.c
+++ franken_buffer.c
@@ -3,6 +3,7 @@ struct FKNBUFFER *buffer_setup(struct FKNBUFFER *storage){
 
   for (i=0; i<OUTTER_BUFF_SIZE; i++)
   {
+      // Link next item, or wrap-round to start if at the end
       storage[i].next = (i==OUTTER_BUFF_SIZE-1) ? storage : &(storage[i+1]);
       storage[i].head =  storage;
       storage[i].buffer = malloc(sizeof(struct FKN_RINGBUFFER));
@@ -12,11 +13,13 @@ struct FKNBUFFER *buffer_setup(struct FKNBUFFER *storage){
   return storage;
 }
 
+// LOOK - No ifs or buts!
 void buffer_add(struct FKNBUFFER **buffer, unsigned char value){
   (*buffer)->buffer->data[((*buffer)->buffer->index++)&INNER_BUFF_SIZE_MASK] = value;
   (*buffer) = (*buffer)->next;
 }
 
+// Very unoptimised.
 char buffer_get(struct FKNBUFFER *buffer, unsigned int index){
   struct CSBUFFER *buf = &(buffer->head[index % OUTTER_BUFF_SIZE]);
   struct RINGBUFFER *rb = buf->buffer;
