c985681043a4683e57e25a0fef44c319f6b8e222
Colin Waddell <mrcolin@gmail.com>
2014-10-20 19:13:32 +0100

    Update franken_buffer.c
    Fixed spelling

--- franken_buffer.c
+++ franken_buffer.c
@@ -1,10 +1,10 @@
 struct FKNBUFFER *fkn_buffer_setup(struct FKNBUFFER *storage){
   int i;
 
-  for (i=0; i<FKN_OUTTER_BUFF_SIZE; i++)
+  for (i=0; i<FKN_OUTER_BUFF_SIZE; i++)
   {
       // Link next item, or wrap-round to start if at the end
-      storage[i].next = (i==FKN_OUTTER_BUFF_SIZE-1) ? storage : &(storage[i+1]);
+      storage[i].next = (i==FKN_OUTER_BUFF_SIZE-1) ? storage : &(storage[i+1]);
       storage[i].head =  storage;
       storage[i].buffer = malloc(sizeof(struct FKN_RINGBUFFER));
       storage[i].buffer->index = 0;
@@ -21,15 +21,15 @@ void fkn_buffer_add(struct FKNBUFFER **buffer, unsigned char value){
 
 // Very unoptimised.
 char fkn_buffer_get(struct FKNBUFFER *buffer, unsigned int index){
-  struct CSBUFFER *buf = &(buffer->head[index % FKN_OUTTER_BUFF_SIZE]);
+  struct CSBUFFER *buf = &(buffer->head[index % FKN_OUTER_BUFF_SIZE]);
   struct RINGBUFFER *rb = buf->buffer;
-  return rb->data[index / FKN_OUTTER_BUFF_SIZE];
+  return rb->data[index / FKN_OUTER_BUFF_SIZE];
 }
 
 void fkn_buffer_clear(struct FKNBUFFER *buffer){
   unsigned int i, j;
 
-  for (i=0; i<OUTTER_BUFF_SIZE; i++)
+  for (i=0; i<OUTER_BUFF_SIZE; i++)
     for(j=0; j<INNER_BUFF_SIZE; j++)
       buffer->head[i].buffer->data[j] = 0;
 }
